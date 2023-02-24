1c4b62915ca51b85fa03d5fe27506415ee8607f9
SIeepii <pyao477@gmail.com>
2023-02-24 15:15:15 -0500

    added decorators to authorize student or tutor

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .decorators import allowed_users
 
 def index(request):
     return render(request, 'base/index.html')
@@ -9,8 +10,10 @@ def index(request):
 def loginPage(request):
     return render(request, 'base/login.html')
 
+@allowed_users(allowed_roles=['student'])
 def studentPage(request):
     return render(request, 'base/student.html')
 
+@allowed_users(allowed_roles=['tutor'])
 def tutorPage(request):
     return render(request, 'base/tutor.html')
