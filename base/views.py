337ee60c9e9f92acfe65d7b82b47516383b6ffbc
SIeepii <pyao477@gmail.com>
2023-02-24 17:27:44 -0500

    added role selection

--- base/views.py
+++ base/views.py
@@ -10,6 +10,19 @@ def index(request):
     return render(request, 'base/index.html')
 
 def loginPage(request):
+
+    if(request.GET.get('student')):
+        group = Group.objects.get(name='student')
+        request.user.groups.add(group)
+        group = Group.objects.get(name='tutor')
+        request.user.groups.remove(group)
+
+    if(request.GET.get('tutor')):
+        group = Group.objects.get(name='tutor')
+        request.user.groups.add(group)
+        group = Group.objects.get(name='student')
+        request.user.groups.remove(group)
+
     return render(request, 'base/login.html')
 
 @allowed_users(allowed_roles=['student'])
