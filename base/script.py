9c65cb3cc81542dbc08efe503aff756a6e808642
Kayla Nguyen <kaylalngn@gmail.com>
2023-03-17 16:09:32 -0400

    fixing url request and test code

--- base/script.py
+++ base/script.py
@@ -2,11 +2,11 @@ import json
 import requests
 
 # flagList is a list of tuples with flag name and search value
-def url(*flagList):
+def url(flagList):
     url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula." \
           "IScript_ClassSearch?institution=UVA01"
     for item in flagList:
-        url += "&" + item[0] + "=" + item[1]
+        url += "&" + item[0] + "=" + str(item[1])
     r = requests.get(url)
     classList = []
     for c in r.json():
@@ -14,6 +14,12 @@ def url(*flagList):
     return classList
 
 def main():
-    flagList = [("term", 1228)]
-    url(flagList)
+    print("test")
+    flagList = [("term", 1228), ("subject", "CS")]
+    list = url(flagList)
+    for item in list:
+        print(item)
+
+main()
+
 
