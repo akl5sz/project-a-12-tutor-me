da77e0c44e87500b5f8e1d3ccdba584f180fbdd4
Kayla Nguyen <kaylalngn@gmail.com>
2023-03-17 15:47:52 -0400

    url requests with flags function

---
+++ oauth_app/script.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+# flagList is a list of tuples with flag name and search value
+def url(*flagList):
+    url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula." \
+          "IScript_ClassSearch?institution=UVA01"
+    for item in flagList:
+        url += "&" + item[0] + "=" + item[1]
+    r = requests.get(url)
+    classList = []
+    for c in r.json():
+        classList.append((c['descr']))
+    return classList
+
+
+
+def main():
+    flagList = [("term", 1228)]
+    url(flagList)
+
