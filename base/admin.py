0de8c278a4673f2cd3a9cb3fc139182fa57d9a86
arjuntrivedi <arjuntrivedi11@gmail.com>
2023-04-03 18:51:44 -0400

    Possible fix migration error

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
+<<<<<<< HEAD
 from .models import Course, Student, Tutor, CourseTutored
+=======
+from .models import Course, Student, Tutor
+>>>>>>> 244d1d1 (Possible fix migration error)
 #from .models import Profile
 
 # Register your models here.
 
+<<<<<<< HEAD
 models = [Course, Student, Tutor, CourseTutored]
+=======
+models = [Course, Student, Tutor]
+>>>>>>> 244d1d1 (Possible fix migration error)
 admin.site.register(models)
